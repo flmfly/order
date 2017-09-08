c40c29a0788f6834716a57d237b575ddf4c741ce
Jeffrey <zhefengl@princetechs.com>
2017-09-08 18:20:01 +0800

    2017-09-08

--- order/src/main/java/simple/order/support/SalesDeliveryNumberTableColumnHandler.java
+++ order/src/main/java/simple/order/support/SalesDeliveryNumberTableColumnHandler.java
@@ -2,20 +2,30 @@ package simple.order.support;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import simple.config.annotation.support.TableColumnHandler;
+import simple.core.service.BaseService;
 import simple.order.model.DeliveryOrderDetail;
 import simple.order.model.SalesOrderDetail;
 
+@Component
 public class SalesDeliveryNumberTableColumnHandler implements TableColumnHandler {
 
+	@Autowired
+	private BaseService baseService;
+
+	@SuppressWarnings("rawtypes")
 	@Override
 	public Object handle(Object row) {
-		SalesOrderDetail sod = (SalesOrderDetail) row;
+		long id = Long.parseLong(String.valueOf(((HashMap) row).get("id")));
+		SalesOrderDetail sod = this.baseService.getHibernateBaseDAO().get(SalesOrderDetail.class, id);
 
 		Set<DeliveryOrderDetail> dodSet = sod.getDeliveryOrders();
 
